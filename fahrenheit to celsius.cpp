d946ba04e0983c30c888c48d32093990cc026082
Nidhi292007 <nidhibj.ece2024@citchennai.net>
2024-09-22 10:41:42 +0530

    Add files via upload

---
+++ fahrenheit to celsius.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std:
+float conveersion(float n)
+{ 
+ return(n-32.0)*5.0/9.0;
+}
+int main()
+{
+ float n=40;
+ conversion(n);
+ cout<<"Fahrenheit to celsius is"<<n;
+ return 0;
+}
+
+
+
+
+OUTPUT:
+
+Fahrenheit to celsius is4.444
